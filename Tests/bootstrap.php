81dedcb2f305b11d66bd394c0e09253a75873f95
epidoux <eric.pidoux@gmail.com>
2013-08-22 23:40:00 +0200

    update phpunit file

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * This file is part of the Inflexible package.
- *
- * (c) Boris Guéry <http://borisguery.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 $file = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($file)) {
