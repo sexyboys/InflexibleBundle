d9410f48669860943599a264cebb155024ba44f5
epidoux <eric.pidoux@gmail.com>
2013-08-24 12:06:00 +0200

    fix services error

--- DependencyInjection/SexyboysInflexibleExtension.php
+++ DependencyInjection/SexyboysInflexibleExtension.php
@@ -22,7 +22,6 @@ class SexyboysInflexibleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $debug = false;
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
